69693402f0624a6ba98de8e880696dbf7a768477
xujinghua <jinghua.xu@student.uni-tuebingen.de>
2020-04-20 00:06:16 +0200

    sig update

--- lexicon.py
+++ lexicon.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python3
-""" Data Structures and Algorithms for CL III, Assignment 4
-    See <https://dsacl3-2019.github.io/a4/> for detailed instructions.
-    Course:      Data Structures and Algorithms for CL III - WS1920
-    Assignment:  lab 04, ex 02
-    Author:      Jinghua Xu
-    Description: An Lexicon class reads a list of words from a given file, uses the FSA class as a way to insert all words into a trie.
- 
-    Honor Code:  I pledge that this program represents my own work.
 """
-
+An Lexicon class reads a list of words from a given file, uses the FSA class as a way to insert all words into a trie.
+by Jinghua Xu
+"""
 import argparse
 from fsa import FSA
 
